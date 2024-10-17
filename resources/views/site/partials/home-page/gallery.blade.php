b100d5960e8bb3da3b27d990533be449f1530d2f
Mostafa Elbagory <zxsofazx@gmail.com>
2024-10-17 09:31:25 +0300

    update gallery style

--- resources/views/site/partials/home-page/gallery.blade.php
+++ resources/views/site/partials/home-page/gallery.blade.php
@@ -1,31 +1,39 @@
+<style>
+    .edu-gallery-area {
+        margin: 20px; /* هامش حول الـ div الكبيرة */
+    }
+    .thumbnail img {
+        width: 45rem;
+    height: 45rem;
+    object-fit: cover;
+    }
+</style>
+
 <div class="edu-gallery-area edu-section-gap" style="{{ $locale === 'ar' ? 'direction: rtl;' : 'direction: ltr;' }}">
     <div class="container">
         <div class="isotope-wrapper">
             <div class="isotop-button button-transparent isotop-filter">
-                <button data-filter="*" class="is-checked">
-                    <span class="filter-text">All</span>
+                <button data-filter="*" class="is-checked"><span class="filter-text">All</span></button>
+                @foreach($categories as $category)
+                <button data-filter=".{{ $category->slug }}">
+                    <span class="filter-text">{{ $category->name_en }}</span>
                 </button>
-                @foreach ($categories as $category)
-                    <button data-filter=".{{ $category->slug }}">
-                        <span class="filter-text">{{ $category->name_en }}</span>
-                    </button>
                 @endforeach
             </div>
-            <div class="isotope-list gallery-grid-wrap">
-                <div id="animated-thumbnials" lg-uid="lg0">
-                    @foreach ($categories as $category)
-                        @foreach ($category->galleries as $gallery)
-                            <a href="{{ asset('public/storage/' . $gallery->image) }}"
-                                class="edu-popup-image edu-gallery-grid p-gallery-grid-wrap isotope-item {{ $category->slug }}"
-                                lg-event-uid="{{ $loop->iteration }}">
-                                <div class="thumbnail">
-                                    <img src="{{ asset('public/storage/' . $gallery->image) }}" width="360px"
-                                        height="360px" alt="Gallery Image">
-                                </div>
-                            </a>
-                        @endforeach
+            <div class="row">
+                @foreach($categories as $category)
+                    @foreach($category->galleries as $gallery)
+                    <div class="col-md-4 mb-4 {{ $category->slug }}">
+
+                        <a href="{{ asset('/back-end/storage/' . $gallery->image) }}" class="edu-popup-image edu-gallery-grid p-gallery-grid-wrap isotope-item" lg-event-uid="{{ $loop->iteration }}">
+                            <div class="thumbnail">
+                                <img src="{{ asset('/back-end/storage/' . $gallery->image) }}" alt="Gallery Image" class="img-fluid">
+                            </div>
+                        </a>
+                        
+                    </div>
                     @endforeach
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
