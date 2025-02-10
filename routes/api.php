c5a81408743a4cfeb7cec7c0e39484776799dbbf
Mostafa Elbagory <zxsofazx@gmail.com>
2025-02-10 20:11:53 +0200

    ssdsdds

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// sofa api
 
 Route::get('/home/{locale}', [ApiController::class, 'home'])->name('api.home');
 Route::get('part-two/{locale}', [ApiController::class, 'categories'])->name('api.categories');
