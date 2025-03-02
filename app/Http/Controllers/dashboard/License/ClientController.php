acfbb0e48927c025f655bb74195af3946876716d
Mostafa Elbagory <zxsofazx@gmail.com>
2025-03-02 16:45:59 +0200

    fix

--- app/Http/Controllers/dashboard/License/ClientController.php
+++ app/Http/Controllers/dashboard/License/ClientController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class ClientController extends Controller
 {
